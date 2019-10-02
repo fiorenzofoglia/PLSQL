ea3dc095c4e350ec0af373f91ef46f9be1717f4a
fiorenzofoglia <55786122+fiorenzofoglia@users.noreply.github.com>
2019-10-02 11:28:42 +0200

    Update PACKAGE_DA_TESTARE.sql

--- PACKAGE_DA_TESTARE.sql
+++ PACKAGE_DA_TESTARE.sql
@@ -1,10 +1,11 @@
-create or replace PACKAGE     UT3.PACKAGE_DA_TESTARE AS 
+create or replace PACKAGE UT3.PACKAGE_DA_TESTARE AS 
 
   procedure moltiplica(add1 in number, add2 in number, ris out number);
 
 END PACKAGE_DA_TESTARE;
 /
-create or replace PACKAGE BODY         PACKAGE_DA_TESTARE IS 
+
+create or replace PACKAGE BODY UT3.PACKAGE_DA_TESTARE IS 
 
   procedure moltiplica(add1 in number, add2 in number, ris out number)
     AS

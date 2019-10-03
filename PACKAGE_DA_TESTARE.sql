aa34bd20a08e9f895c585936abe8ec1f6460fc6e
fiorenzofoglia <55786122+fiorenzofoglia@users.noreply.github.com>
2019-10-03 09:41:02 +0200

    Update PACKAGE_DA_TESTARE.sql

--- PACKAGE_DA_TESTARE.sql
+++ PACKAGE_DA_TESTARE.sql
@@ -1,6 +1,7 @@
 create or replace PACKAGE UT3.PACKAGE_DA_TESTARE AS 
  
 --inserito commento
+--inserito secondo commento
   procedure moltiplica(add1 in number, add2 in number, ris out number);
 
 END PACKAGE_DA_TESTARE;

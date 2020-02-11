e6e3e0b4aaa405f4b8a8093b7ba4089028d887b4
fiorenzofoglia <55786122+fiorenzofoglia@users.noreply.github.com>
2020-02-11 13:00:09 +0100

    Update execute_tests.sh

--- execute_tests.sh
+++ execute_tests.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 rm report.xml
+rm temp.txt
 
 sqlplus -s sys/GetStarted18c@//localhost:1521/XE as sysdba <<EOF
 set serveroutput on;
@@ -9,3 +10,5 @@ execute ut3.ut.run('SYS', ut_xunit_reporter());
 SPOOL OFF
 quit;
 EOF
+
+head -n -1 report.xml > temp.txt ; mv temp.txt report.xml

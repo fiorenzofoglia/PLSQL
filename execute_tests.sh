bb71b1be14ed729ae5af15d331464d982e7f56bc
fiorenzofoglia <55786122+fiorenzofoglia@users.noreply.github.com>
2020-02-11 11:32:37 +0100

    Update execute_tests.sh

--- execute_tests.sh
+++ execute_tests.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
 
-for filename in *_TEST.sql
-do
-echo "Executing file $filename..."
 sqlplus -s sys/GetStarted18c@//localhost:1521/XE as sysdba <<EOF
-execute ut3.ut.run('$filename');
+execute ut3.ut.run('SYS');
 quit;
 EOF
-done

5edd18f3c584515dedda01b3b5a3f7e7c67072a0
fiorenzofoglia <55786122+fiorenzofoglia@users.noreply.github.com>
2020-02-11 11:41:23 +0100

    Update execute_tests.sh

--- execute_tests.sh
+++ execute_tests.sh
@@ -5,6 +5,6 @@ rm report.xml
 sqlplus -s sys/GetStarted18c@//localhost:1521/XE as sysdba <<EOF
 set serveroutput on;
 SPOOL report.xml
-execute ut3.ut.run('SYS');
+execute ut3.ut.run('SYS', ut_xunit_reporter());
 quit;
 EOF

73a2ec9d8d97c33b0ce3f5fcf02607def2f9266d
fiorenzofoglia <55786122+fiorenzofoglia@users.noreply.github.com>
2020-02-11 10:53:45 +0100

    Add files via upload

---
+++ execute_tests.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+for filename in *_TEST.sql
+do
+echo "Executing file $filename..."
+sqlplus -s sys/GetStarted18c@//localhost:1521/XE as sysdba <<EOF
+ut3.ut.run('$filename');
+quit;
+EOF
+done

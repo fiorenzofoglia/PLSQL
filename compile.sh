9b4c3c1a426a419d526597b74ba6337dc55aa34a
fiorenzofoglia <55786122+fiorenzofoglia@users.noreply.github.com>
2020-02-12 12:05:08 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 for filename in *.sql
 do
 echo "Executing file $filename..."
-sqlplus -s FFOGLIA/Piaggine_3801096736//localhost:1521/XE as sysdba <<EOF
+sqlplus FFOGLIA/Piaggine_3801096736@//localhost:1521/XE <<EOF
 @$filename;
 quit;
 EOF

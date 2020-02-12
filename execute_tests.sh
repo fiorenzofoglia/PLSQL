610fd7afaaa92e74eff0c1d8eff462660b60036f
fiorenzofoglia <55786122+fiorenzofoglia@users.noreply.github.com>
2020-02-12 12:51:09 +0100

    Update execute_tests.sh

--- execute_tests.sh
+++ execute_tests.sh
@@ -3,10 +3,12 @@
 rm report.xml
 rm temp.txt
 
-sqlplus sys/Piaggine_3801096736@//localhost:1521/XE as sysdba <<EOF
+sqlplus -s sys/Piaggine_3801096736@//localhost:1521/XE as sysdba <<EOF
 set serveroutput on;
+SPOOL report.xml;
 execute ut3.ut.run('FFOGLIA', ut_xunit_reporter());
+SPOOL OFF;
 quit;
-EOF >> report.xml
+EOF
 
 head -n -3 report.xml > temp.txt ; mv temp.txt report.xml
